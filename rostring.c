f35a18b92e3ff0100ae8f78560d1000c49a0d091
Berniece Buttermilk <bbutterm@su-a1.21-school.ru>
2020-01-25 01:28:34 +0300

    day11

--- rostring.c
+++ rostring.c
@@ -1,47 +1,53 @@
 #include <unistd.h>
+
 int ft_isspace(char c)
 {
 	return (c == ' ' || c == '\t');
 }
+
 void rostring(char *s)
 {
-	int printed = 0;
 	int i = 0;
-	//skip 1st word
+	int first = 0;
+
+	while (s[i])
+	{
+		while (ft_isspace(s[i]))
+			i++;
+		if (s[i] && !ft_isspace(s[i]))
+		{
+			if (first == 0)
+			{
+				while (s[i] && !ft_isspace(s[i]))
+				{
+					i++;
+					first++;
+				}
+			}
+			else
+			{
+				while (s[i] && !ft_isspace(s[i]))
+				{
+					write(1,&s[i],1);
+					i++;
+				}
+				write(1," ",1);
+			}
+		}
+	}
+	i = 0;
 	while (ft_isspace(s[i]))
 		i++;
-	while (!ft_isspace(s[i]) && s[i])
-		i++;
-	if (ft_isspace(s[i]) && s[i])
-		i++;
-	while(s[i])
-	{
-		if (ft_isspace(s[i]))
-		{
-			write(1,&s[i],1);
-			while(ft_isspace(s[i]))
-				i++;
-			printed++;
-		}
-		write(1,&s[i],1);
-		i++;
-	}
-	if (!s[i] && printed > 0)
-		write(1," ",1);
-	i = 0;
-	while (ft_isspace(s[i]) && s[i])
-		i++;
-	while (!ft_isspace(s[i]) && s[i])
+	while (first > 0)
 	{
 		write(1,&s[i],1);
 		i++;
+		first--;
 	}
-	write(1,"\n",1);
 }
 int main(int ac, char **av)
 {
-	if (ac < 2)
-		write(1,"\n",1);
-	else 
+	if (ac > 1)
 		rostring(av[1]);
+	write(1,"\n",1);
 }
